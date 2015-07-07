657b619e375a381a02f1b10b982a9600c48cf8e8
Will Welch <willcwelch@gmail.com>
2015-07-07 13:41:24 -0400

    Added comments and zip code extraction fixes.

--- twitterbot.js
+++ twitterbot.js
@@ -1,75 +1,88 @@
 (function() {
   
-  // modules
+  // Import the necessary modules.
   require('./lib/sugar-dates');
-
   var Twit = require('twit');
   var Geocoder = require('geocoder');
   var AlchemyAPI = require('alchemyapi_node');
   var TwitKey = require('twit/api_key');
   
-  // config vars
+  // Set the user ID for the account we're tweeting from.
   var userId = 3331300337;
 
-  // setup
+  // Instantiate Twit and AlchemyAPI.
   var twitter = new Twit(TwitKey);
   var alchemyapi = new AlchemyAPI();
-  // Connect to Twitter and look for tweets inlcuding '@welch_test'
+
+  // Connect to Twitter and look for tweets inlcuding '@welch_test'.
   var stream = twitter.stream('statuses/filter', {track: '@welch_test'});
   
+  // Handle the tweet if one comes through on the stream.
   stream.on('tweet', function (tweet) {
-  	// Log if it was sent from the current acount, or if the account was only mentioned
+
+  	// Check if the tweet was sent from the current acount, or if the account was only mentioned.
     if (tweet.in_reply_to_user_id !== userId && tweet.user.id !== userId) {
       console.log('Mentioned or my own tweet: ', tweet.text);
+    // Check if the tweet is about weather.
+    } else if (/weather/i.test(tweet.text) || /forecast/i.test(tweet.text)) {
+      // Create an object with the data from the tweet.
+      var tweetData = Object.create(TweetData);
+      tweetData.constructor(tweet, function (err, response) {
+
+        // Handle any errors from creating the data object.
+        if (err) {
+          console.log(err.message);
+        } else {
+
+          // Extract a zip code.
+          ForecastController.getZip(response, function (err, response) {
+
+            // Handle any errors from exptracting the zip code.
+            if (err) {
+              console.log(err);
+            // Log the zip code.
+            } else {
+              console.log(response);
+            }
+
+          });
+
+        }
+
+      });
+    // If the tweet was a reply but we don't know what it's about, ignore it.
     } else {
-
-      if (/weather/i.test(tweet.text) || /forecast/i.test(tweet.text)) {
-        var tweetData = Object.create(TweetData);
-        tweetData.constructor(tweet, function (err, response) {
-
-          if (err) {
-            console.log(err.message);
-          } else {
-            ForecastController.getZip(response, function (err, response) {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log(response);
-              }
-            });
-          }
-
-        });
-      } else {
-        console.log('Not sure what this is about.');
-      }
-
+      console.log('Not sure what this is about.');
     }
+
   });
 
-  // Log if there is an error
+  // Log if Twitter sends an error.
   stream.on('error', function (error) {
     console.log('Whoops, there was a problem: ', error.message);
   });
 
-  // Log if the Twitter rate limit is reached
+  // Log if the Twitter rate limit is reached.
   stream.on('limit', function (limitMessage) {
   	console.log('Hmmm, looks like we are violating the rate limit: ', limitMessage);
   });
   
-  // Log if Twitter disconnects
+  // Log if Twitter disconnects.
   stream.on('disconnect', function (disconnectMessage) {
   	console.log('We were disconnected: ', disconnectMessage);
   });
   
-  // Log attempts to reonnect
+  // Log attempts to reonnect to Twitter.
   stream.on('reconnect', function (request, response, connectInterval) {
     console.log('Trying to reconnect');
     console.log('Request: ', request);
     console.log('Response: ', response);
   });
 
+  // Object that takes a tweet and parses out the data we need in a useable format.
   var TweetData = {
+
+    // Takes a tweet and extracts the text, date and location.
     constructor: function(tweet, callback) {
       var that = this;
 
@@ -77,8 +90,10 @@
       this.setDate(tweet);
       this.setCity(tweet, function(err, response) {
 
+        // Send an error if a location couldn't be found.
         if (err) {
-          callback(err, that);
+          callback(err);
+        // If all is well, send back the newly constructed object.
         } else {
           callback(null, that);
         }
@@ -86,38 +101,49 @@
       });
     },
 
-    // Sets 'city' to {[value], [type], [parsed]}
+    // Sets 'city' to {[value], [type], [parsed]}.
     setCity: function(tweet, callback) {
-      var  cities, that = this;
+      var  cities, latlng = [], that = this;
 
-      // look for zip code in the format 00000-0000 or 00000
+      // Look for zip code in text with the format 00000-0000 or 00000.
       cities = tweet.text.match(/^\d{5}(?:[-\s]\d{4})?/);
 
+      // If a zip code is found, set it as the city.
       if (cities) {
         this.city = {value: cities[0], type: 'zip', parsed: true};
         callback(null, this.city);
+      // If a zip code isn't found, try to find a city in the text using AlchemyAPI.
       } else {
         alchemyapi.entities('text', tweet.text, {}, function (response) {
           
+          // If AlchemyAPI sends an error, make the cities array empty.
           if (response.status === 'ERROR') {
             cities = [];
+          // If AlchemyAPI sends a response, filter it for entities that are cities and add them to the cities array.
+          // TODO: add State abbreviation if it exists. Try NY if it doesn't exist.
           } else if (response.status === 'OK') {
-            // Filter for the entities that are cities.
             cities = response.entities.filter(function (value) {
               return value.type === 'City';
             });
           }
 
+          // If there are cities from Alchemy API, send the first result.
           if (cities.length > 0) {
             that.city = {value: cities[0].text, type: 'name', parsed: true};
+            callback(null, that.city);
+          // If there are no cities from AlchemyAPI but the tweet has a location, send that as the city.
           } else if (tweet.coordinates !== null) {
-            that.city = {value: tweet.coordinates.coordinates, type: 'latlon', parsed: false};
+            // Since Twitter returns coordinates as an array in the format [lng,lat], reorder it to [lat,lng].
+            latlng[0] = tweet.coordinates.coordinates[1];
+            latlng[1] = tweet.coordinates.coordinates[0];
+            that.city = {value: latlng, type: 'latlng', parsed: false};
+            callback(null, that.city);
+          // If there is no location information, send an error.
           } else { 
             that.city = null;
             callback(new Error('No location data available.'));
           }
-
-          callback(null, that.city);
+          
         });
       }
 
@@ -127,68 +153,104 @@
     setDate: function(tweet) {
       var date;
 
+      // Try to create a date from the tweet text using Sugar's loose date parsing.
+      // TODO: add functionality for recognizing morning, afternoon, night, evening, etc for times.
       date = Date.create(tweet.text, true);
 
+      // If Sugar couldn't create a date, set the date to the time the tweet was sent.
       if (isNaN(date.valueOf())) {
         date = new Date(tweet.created_at);
-        this.date = {value: date, parsed: false};   
+        this.date = {value: date, parsed: false};  
+      // If Sugar did create a date, set it as the date value. 
       } else {
         this.date = {value: date, parsed: true};
       } 
 
     },
 
+    // Logs the data properties
     printValues: function() {
       console.log(this.city, this.date, this.text);
     }
   }
 
+  // Object that takes tweet data and uses it to pull a forecast from memcached.
   var ForecastController = {
-    getZip: function(data, callback) {
-      var zip, zips, latlon, name;
 
+    // Takes tweet data and sends back a postal code
+    getZip: function(data, callback) {
+      var zips, latlng, name;
+
+      // If the data type is alread a zip code, send that.
       if (data.city.type === 'zip') {
         callback(null, data.city.value);
-      } else if (data.city.type === 'latlon') {
-        latlon = data.city.value;
-        Geocoder.reverseGeocode(latlon[1], latlon[0], function (err, response) {
+      // If the data type is a latitude and longitude, let Geocoder parse it.
+      } else if (data.city.type === 'latlng') {
+        latlng = data.city.value;
+        // Send the latituted and longitude to Google through Geocoder.
+        Geocoder.reverseGeocode(latlng[0], latlng[1], function (err, response) {
           
+          // If Google sends an error, return the error.
           if (err) {
             callback(err);
+          // If Google sends a response, filter it for a postal code.
           } else {
             zips = response.results[0].address_components.filter(function (value) {
               return value.types[0] === 'postal_code';
             });
-            callback(null, zips[0].long_name);
+
+            // If there are postal codes, send the first one.
+            if (zips) {
+              callback(null, zips[0].long_name);
+            // If there are no postal codes, send an error.
+            } else {
+              callback(new Error('Could not find a zip code.'))
+            }
           }
 
         });
+      // If the data type is the name of a city, try very hard to turn it into a postal code.
       } else if (data.city.type === 'name') {
         name = data.city.value;
+        // Geocode the name to a latitude and longitude to make it easier to pull a specific postal code.
         Geocoder.geocode(name, function (err, response) {
           
+          // If Google sends back an error, return the error.
           if (err) {
             callback(err);
+          // If Google sends a response, extract the latitude and longitude.
           } else {
-            latlon = [];
-            latlon[0] = response.results[0].geometry.location.lat;
-            latlon[1] = response.results[0].geometry.location.lng;
+            latlng = [];
+            latlng[0] = response.results[0].geometry.location.lat;
+            latlng[1] = response.results[0].geometry.location.lng;
 
-            Geocoder.reverseGeocode(latlon[0], latlon[1], function (err, response) {
+            // Send the latituted and longitude to Google through Geocoder.
+            Geocoder.reverseGeocode(latlng[0], latlng[1], function (err, response) {
             
+              // If Google sends an error, return the error.
               if (err) {
                 callback(err);
+              // If Google sends a response, filter it for a postal code.
               } else {
                 zips = response.results[0].address_components.filter(function (value) {
                   return value.types[0] === 'postal_code';
                 });
-                callback(null, zips[0].long_name);
+
+                // If there are postal codes, send the first one.
+                if (zips) {
+                  callback(null, zips[0].long_name);
+                // If there are no postal codes, send an error.
+                } else {
+                  callback(new Error('Could not find a zip code.'))
+                }
+
               }
 
             });
           }
 
         });
+      // If the data is a type we don't recognize, send an error.
       } else {
         callback(new Error('Location type not recognized.'));
       }
